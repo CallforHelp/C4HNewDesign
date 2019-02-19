416901cb6455e93f9fd992e2d95a49ff555fd89b
HelmiB2 <34322239+HelmiB2@users.noreply.github.com>
2019-02-19 16:12:16 +0100

    rotation

--- src/c4h/C4hController.java
+++ src/c4h/C4hController.java
@@ -1,8 +1,11 @@
 package c4h;
 
+import javafx.animation.Animation;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class C4hController {
 	@FXML
@@ -16,6 +19,12 @@ public class C4hController {
 	@FXML
 	private void initialize() {
 	//	myExitButton.setGraphic(new ImageView(imageDecline));
+		RotateTransition rotation = new RotateTransition(Duration.seconds(0.5), myExitButton);
+		rotation.setCycleCount(Animation.INDEFINITE);
+		rotation.setByAngle(360);
+
+		myExitButton.setOnMouseEntered(e -> rotation.play());
+		myExitButton.setOnMouseExited(e -> rotation.pause());
 		myExitButton.setOnAction((event) -> {
 			System.out.println("Button Action\n");
 			System.exit(0);
