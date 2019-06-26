f3cb89146b3415ebd748abc2db82ca2a1f806bfc
HelmiB2 <34322239+HelmiB2@users.noreply.github.com>
2019-06-26 13:11:27 +0200

    design

--- src/c4h/downloads/WebControler.java
+++ src/c4h/downloads/WebControler.java
@@ -1,13 +1,25 @@
 package c4h.downloads;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,10 +27,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogEvent;
+import javafx.scene.control.DialogPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.PopupFeatures;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import javafx.util.Callback;
 import javafx.util.Duration;
+
+
+
+import javax.swing.JOptionPane;
 
 public class WebControler  implements Initializable{
 
@@ -59,10 +82,94 @@ public class WebControler  implements Initializable{
 	private void loadBrowser() {
 	
          webkit = browser.getEngine();
-         webkit.load("https://fehlermeldung.3s-hamburg.de/hilfe/");
-         
-    }
-     
-	
+         webkit.load("https://fehlermeldung.3s-hamburg.de/downloads");
+         download();
+       
 
+	}
+	@SuppressWarnings("unused")
+	@FXML
+	private void download() {
+		  browser.getEngine().locationProperty().addListener(new ChangeListener<String>() {
+
+			
+				int readBytes;
+		
+				@Override
+				
+				public void changed(ObservableValue<? extends String> observable, String oldValue, String newLoc) {
+					if (newLoc.contains("upload")) {
+			            
+						FileChooser chooser = new FileChooser();
+			            chooser.setTitle("Save " + newLoc);
+			    	
+					
+						String fileName;
+			 
+						File saveFile = chooser.showSaveDialog(button.getScene().getWindow());
+						  int b = 0;
+			            if (saveFile != null) {
+			            	 BufferedInputStream  is = null;
+			                 BufferedOutputStream os = null;
+			                 try {
+			                     try {
+									is = new BufferedInputStream(new URL(newLoc).openStream());
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			                     try {
+									os = new BufferedOutputStream(new FileOutputStream(saveFile));
+								} catch (FileNotFoundException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+
+			                     try {
+									while ((readBytes = is.read()) != -1) {
+									 os.write(b);
+									 }
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			                 } finally {
+			                     try { if (is != null) is.close(); } catch (IOException e) {}
+			                     try { if (os != null) os.close(); } catch (IOException e) {}
+			                 }
+			            }	
+			        
+			            
+			        }
+					
+					
+					
+				}
+			});
+		/*
+		 * BufferedInputStream  is = null;
+			                BufferedOutputStream os = null;
+			                try {
+			                    is = new BufferedInputStream(new URL(newLoc).openStream());
+			                    os = new BufferedOutputStream(new FileOutputStream(saveFile));
+
+			                    
+								while ((readBytes = is.read()) != -1) {
+								byte[] b = null;
+								os.write(b);
+			                    }
+			                } catch (MalformedURLException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} finally {
+			                    try { if (is != null) is.close(); } catch (IOException e) {}
+			                    try { if (os != null) os.close(); } catch (IOException e) {}
+			                }
+*/
+		
+		
+	}
 }
