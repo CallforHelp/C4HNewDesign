845577b5a436c25fb73264ca0ba1fea0e286fa75
HelmiB2 <bani.hel@gmail.com>
2024-05-23 14:47:47 +0200

    rename File ED04.png

--- src/c4h/PcInformation/pcInformation.java
+++ src/c4h/PcInformation/pcInformation.java
@@ -213,7 +213,7 @@ public class pcInformation {
 				// Überprüfe, ob die dritte Zeile erreicht ist
 				if (lineCount == 3) {
 					// Gib die dritte Zeile aus
-					//Modell="ED05";
+					Modell="ED04";
 					return Modell;
 				}
 			}
