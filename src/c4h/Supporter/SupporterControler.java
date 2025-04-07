63e8d85b10c73cc37a460c59b5002c656f030f50
HelmiB2 <bani.hel@gmail.com>
2025-04-07 13:25:05 +0200

    create a new Browser

--- src/c4h/Supporter/SupporterControler.java
+++ src/c4h/Supporter/SupporterControler.java
@@ -1,20 +1,28 @@
 package c4h.Supporter;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,10 +35,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.util.Duration;
 
 public class SupporterControler implements Initializable {
 
+	//public String URL = "https://fehlermeldung.3s-hamburg.de/mitarbeiter/";
+	public String URL = "https://www.google.de";
 	@FXML
 	private AnchorPane supporterContainer;
 	@FXML
@@ -41,13 +53,18 @@ public class SupporterControler implements Initializable {
 	private String path ="/config";
 	@FXML
 	private ImageView imageLogo;
+	@FXML
+	private AnchorPane browserContainer;
+	@FXML
+	private WebView  browser = new WebView();
+	@FXML
+	private WebEngine webkit;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		System.out.println("SupportControler");
-		LogoImage();
-
+		//LogoImage();
 	}
 	/**
 	 * Setzt das Bild für das ImageView.
@@ -105,6 +122,7 @@ public class SupporterControler implements Initializable {
 				parentContainer.getChildren().remove(supporterContainer);
 			});
 			timeline.play();
+			loadBrowser();
 		} else {
 			// Passwort ist falsch
 			Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -161,6 +179,94 @@ public class SupporterControler implements Initializable {
 		});
 
 		currentSceneTimeline.play();
+	}
+	@FXML
+	private void loadBrowser() {
+		System.out.println("browserstart");
+
+		try {
+			// TrustManager-Array initialisieren, um das Serverzertifikat zu überprüfen
+			TrustManager[] trustAllCerts = new TrustManager[]{
+					new X509TrustManager() {
+						public X509Certificate[] getAcceptedIssuers() {
+							return null;
+						}
+
+						public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+						public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+							for (@SuppressWarnings("unused") X509Certificate cert : certs) {
+								// Hier können Sie die Zertifikatsprüfung anpassen, z. B. Überprüfung des Ausstellers, Gültigkeitszeitraums usw.
+								// Beispiel: cert.getIssuerDN().getName() für den Aussteller
+								// Beispiel: cert.getNotAfter() für das Ablaufdatum
+							}
+						}
+					}
+			};
+
+			// SSL-Kontext initialisieren
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+			// SSL-Socket-Fabrik initialisieren
+			HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+			// Verbindung zur URL herstellen
+			URL url = new URL(URL);
+			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+
+			// Antwortcode abrufen
+			int responseCode = connection.getResponseCode();
+			System.out.println("Response Code: " + responseCode);
+			connection.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// JavaScript aktivieren
+		webkit = browser.getEngine();
+		webkit.setJavaScriptEnabled(true);
+
+		// User-Agent setzen
+		webkit.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36");
+
+		// Fehlerbehandlung für WebView hinzufügen
+		webkit.setOnError(event -> {
+			System.out.println("Fehler beim Laden der Seite: " + event.getMessage());
+		});
+
+		String userDataDirectory=System.getProperty("user.home") + File.separator + "AppData" + File.separator + "Roaming"+"\\c4h.MainC4H\\webview";
+		System.out.println(userDataDirectory);
+		System.setProperty("user.home", userDataDirectory);
+
+		// Ereignis zum Überwachen des Ladezustands der WebEngine
+		webkit.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue == Worker.State.SUCCEEDED) {
+				// Wenn die Seite geladen ist, Lazy-Loading von Bildern implementieren
+				webkit.executeScript(
+						"var lazyImages = document.querySelectorAll('img[data-src]');" +
+								"lazyImages.forEach(function(img) {" +
+								"  img.setAttribute('src', img.getAttribute('data-src'));" +
+								"  img.onload = function() {" +
+								"    img.removeAttribute('data-src');" +
+								"  };" +
+								"});"
+						);
+
+				System.out.println("Webseite erfolgreich geladen.");
+			} else if (newValue == Worker.State.FAILED) {
+				System.out.println("Fehler beim Laden der Webseite.");
+			}
+		});
+
+		// Laden der URL in den WebView
+		try {
+			webkit.load(URL);
+			System.out.println(URL);
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
